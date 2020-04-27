c7b8ba6f80a8608e6fc8f38338094b69a80f4cb3
simon.montaggioni <simon.montaggioni@gmail.com>
2020-04-27 17:14:00 -0400

    feat: Add TeslaCar component  and install PropTypes.

--- src/containers/TeslaBattery/TeslaBattery.js
+++ src/containers/TeslaBattery/TeslaBattery.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './TeslaBattery.css';
 
 import TeslaNotice from '../../components/TeslaNotice/TeslaNotice';
+import TeslaCar from '../../components/TeslaCar/TeslaCar';
 
 class TeslaBattery extends React.Component {
   render() {
     return (
       <form className='tesla-battery'>
         <h1>Range Per Charge</h1>
+        <TeslaCar wheelsize={21} />
         <TeslaNotice />
       </form>
     );
