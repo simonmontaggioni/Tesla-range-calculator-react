2a9b7c2e834610306f7de2fc47f9536180d87f1a
simon.montaggioni <simon.montaggioni@gmail.com>
2020-04-27 17:21:34 -0400

    feat: Set the app state into TeslaBattery component,

--- src/containers/TeslaBattery/TeslaBattery.js
+++ src/containers/TeslaBattery/TeslaBattery.js
@@ -5,11 +5,25 @@ import TeslaNotice from '../../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../../components/TeslaCar/TeslaCar';
 
 class TeslaBattery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carStats: [],
+      config: {
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels: 19,
+      },
+    };
+  }
+
   render() {
+    const { config } = this.state;
     return (
       <form className='tesla-battery'>
         <h1>Range Per Charge</h1>
-        <TeslaCar wheelsize={21} />
+        <TeslaCar wheelsize={config.wheels} />
         <TeslaNotice />
       </form>
     );
