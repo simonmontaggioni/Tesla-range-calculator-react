1a52e566bd43de77ab5090142d3d8148100a67e1
simon.montaggioni <simon.montaggioni@gmail.com>
2020-04-27 19:38:25 -0400

    feat: Calculate tesla stats from data service.

--- src/containers/TeslaBattery/TeslaBattery.js
+++ src/containers/TeslaBattery/TeslaBattery.js
@@ -5,6 +5,8 @@ import TeslaNotice from '../../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../../components/TeslaCar/TeslaCar';
 import TeslaStats from '../../components/TeslaStats/TeslaStats';
 
+import { getModelData } from '../../services/BatteryService';
+
 class TeslaBattery extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +19,35 @@ class TeslaBattery extends React.Component {
         wheels: 19,
       },
     };
+
+    this.calculateStats = this.calculateStats.bind(this);
+    this.statsUpdate = this.statsUpdate.bind(this);
+  }
+
+  calculateStats = (models, value) => {
+    const dataModels = getModelData();
+    return models.map((model) => {
+      const { speed, temperature, climate, wheels } = value;
+      const miles =
+        dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][
+          temperature
+        ];
+      return {
+        model,
+        miles,
+      };
+    });
+  };
+
+  statsUpdate() {
+    const carModels = ['60', '60D', '75', '75D', '90D', 'P100D'];
+    this.setState({
+      carStats: this.calculateStats(carModels, this.state.config),
+    });
+  }
+
+  componentDidMount() {
+    this.statsUpdate();
   }
 
   render() {
