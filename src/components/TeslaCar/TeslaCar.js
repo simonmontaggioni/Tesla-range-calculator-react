8eb6ee18e74c1f0321aacc7aa316214d627e4cef
simon.montaggioni <simon.montaggioni@gmail.com>
2020-04-30 10:10:14 -0400

    style: Add bouncing animation to the tesla wheels.

--- src/components/TeslaCar/TeslaCar.js
+++ src/components/TeslaCar/TeslaCar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './TeslaCar.css';
-const TeslaCar = (props) => (
-  <div className='tesla-car tesla-car-animation'>
-    <div className='tesla-wheels tesla-wheels-animation'>
+const TeslaCar = (props) => {
+  console.log(props.wheelSize);
+  return (
+    <div className='tesla-car tesla-car-animation'>
       <div
-        className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelSize}`}
-      ></div>
-      <div
-        className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelSize}`}
-      ></div>
+        className={`tesla-wheels tesla-wheel--animation--${props.wheelSize}`}
+      >
+        <div
+          className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelSize}`}
+        ></div>
+        <div
+          className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelSize}`}
+        ></div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 TeslaCar.propTypes = {
   wheelSize: PropTypes.number,
